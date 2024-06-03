a5ed248e446faa492de99ddee20129e03aba986b
Earthchild2 <nthabiseng.fakude2@gmail.com>
2024-06-03 09:51:26 +0200

    made calculater

---
+++ main.js
@@ -0,0 +1,45 @@
+let num1 = prompt ('enter the first number ');
+let op = prompt ('enter your operator');
+let num2 =prompt('enter your second number');
+
+
+
+
+if (op ==='+'){
+
+console.log('your answer is' + (num1 + num2));
+}
+
+else if (op ==='-'){
+
+ console.log (num1 - num2);
+}
+
+else if (op ==='*'){
+    console.log(num1 * num2);
+}
+
+
+
+else if (op ==='/'){
+    console .log (num1 / num2);
+
+}
+
+ else {console .log ('invalid operator')}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
